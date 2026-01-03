22ca8fa5e5772607c9481ff15b43ea67d284ada1
Abhijith P <abhijithp@Abhijiths-MacBook-Air.local>
2026-01-03 19:45:03 +0530

    created views file

--- inventory/views.py
+++ inventory/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from django_filters.rest_framework import DjangoFilterBackend
+from .models import Vehicle, Booking
+from .serializers import VehicleSerializer, BookingSerializer
 
-# Create your views here.
+class VehicleViewSet(viewsets.ModelViewSet):
+    queryset=Vehicle.objects.all()
+    serializer_class=VehicleSerializer
+    filter_backends=[DjangoFilterBackend]
+    filterset_fields= ['brand', 'fuel_type', 'is_available']
+
+class BookingViewSet(viewsets.ModelViewSet):
+    queryset=Booking.objects.all()
+    serializer_class=BookingSerializer
