9915c8fd1c8b1dde225fb5a3340082812457c515
Abhijith P <abhijithp@Abhijiths-MacBook-Air.local>
2026-01-03 19:56:28 +0530

    made urls.py file in the app and configured it. also made changes in the project urls for routing

---
+++ inventory/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from .views import VehicleViewSet,  BookingViewSet
+
+router=DefaultRouter()
+router.register('vehicles', VehicleViewSet, basename='vehicle')
+router.register('bookings', BookingViewSet, basename='booking')
+
+urlpatterns = [
+    path('api/', include(router.urls)),
+]
