3de3dcc0d32c20c695f57b64e1ee4bc7eafadd3f
Abhijith P <abhijithp@Abhijiths-MacBook-Air.local>
2026-01-03 20:34:21 +0530

    coded admin file, created superuser and tested the models from django admin panel

--- inventory/admin.py
+++ inventory/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from .models import Vehicle, Booking
 
-# Register your models here.
+@admin.register(Vehicle)
+class VehicleAdmin(admin.ModelAdmin):
+    list_display = ['name', 'brand', 'year', 'price_per_day', 'fuel_type', 'is_available']
+    list_filter = ['brand', 'fuel_type', 'is_available']
+    search_fields = ['name', 'brand']
+
+@admin.register(Booking)
+class BookingAdmin(admin.ModelAdmin):
+    list_display = ['customer_name', 'vehicle', 'start_date', 'end_date', 'total_amount']
+    list_filter = ['start_date', 'end_date']
+    search_fields = ['customer_name', 'customer_phone']
+    
